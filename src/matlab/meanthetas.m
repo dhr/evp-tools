6917106a96f657319da438532e72840315b9c2ad
Daniel Holtmann-Rice <dholtmannrice@gmail.com>
2012-02-03 02:53:47 -0500

    Fix meanthetas to work properly

--- src/matlab/meanthetas.m
+++ src/matlab/meanthetas.m
@@ -3,6 +3,9 @@ function [thetas confs] = meanthetas(cols)
 cols = max(max(cols, [], 5), [], 4);
 totals = sum(cols, 3);
 nthetas = size(cols, 3);
-anglevals = repmat(shiftdim(0:nthetas - 1, -1), size(totals))/nthetas*pi;
-thetas = sum(bsxfun(@rdivide, cols, totals).*anglevals, 3);
+anglevals = repmat(shiftdim(0:nthetas - 1, -1), size(totals))*2*pi/nthetas;
+normed = bsxfun(@rdivide, cols, totals);
+normed(totals == 0) = 0;
+thetas = atan2(sum(normed.*sin(anglevals), 3), sum(normed.*cos(anglevals), 3))/2;
 confs = max(cols, [], 3);
+thetas(confs == 0) = 0;
