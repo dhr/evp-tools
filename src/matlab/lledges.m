e169029214e8032c8644e6accd0a6d11108fde21
Daniel Holtmann-Rice <dholtmannrice@gmail.com>
2012-08-10 15:57:15 -0400

    Remove MATLAB MEX interface

--- src/matlab/lledges.m
+++
@@ -1,9 +0,0 @@
-function cols = lledges(image)
-
-evp('progmon', 'waitbar');
-h = waitbar(0, 'Logical/Linear...');
-
-out = evp('lledges', single(image));
-cols = circshift(out, [0 0 -size(out, 3)/4 0]);
-
-close(h);
--- src/matlab/lllines.m
+++
@@ -1,9 +0,0 @@
-function cols = lllines(image)
-
-evp('progmon', 'waitbar');
-h = waitbar(0, 'Logical/Linear...');
-
-out = evp('lllines', single(image));
-cols = circshift(out, [0 0 -size(out, 3)/4 0]);
-
-close(h);
