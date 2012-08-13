aebc8beaa5f0ea101a006a47353dae3c34cbd1c3
Daniel Holtmann-Rice <dholtmannrice@gmail.com>
2012-08-12 22:00:43 -0400

    Fix evpsave MATLAB function

--- src/matlab/evpsave.m
+++ src/matlab/evpsave.m
@@ -1,4 +1,4 @@
 function evpsave(file, data)
 
-evpdata = permute(flipdim(single(data), 1), [2 1 3:ndims(in)]); %#ok<NASGU>
+evpdata = permute(flipdim(single(data), 1), [2 1 3:ndims(data)]); %#ok<NASGU>
 save(file, 'evpdata');
